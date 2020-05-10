6c5b65f5eae69b7854fd82cdffc7d831aa913daf
Cedric M'Passi <cedricmpassi@gmail.com>
2020-05-10 16:11:02 +0200

    Working write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use libc::write;
+use libc::c_void;
+
 fn main() {
     println!("Hello, world!");
+
+    let arg: &[u8] = b"hello";
+    let arg = arg.as_ptr() as *const c_void;
+    unsafe {
+        write(1, arg, 4);
+    }
 }
