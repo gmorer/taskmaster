0175464d3864f191424a5bf9407d5feb7e422f58
Cedric M'Passi <cedricmpassi@gmail.com>
2020-05-10 20:29:10 +0200

    Typing issues

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,44 @@ use config::Conf;
 type Error = Box<dyn std::error::Error>;
 
 const CONFIGURATION: &str = "/home/tet/project/taskmaster/example.toml";
+use libc::c_char;
+use libc::execve;
+use libc::fork;
+use libc::pid_t;
+use libc::size_t;
+use libc::strlen;
+use libc::INT_MAX;
+use std::ffi::CString;
+use std::ptr;
+
+fn exec_child(path: &str, argv: Vec<String>) -> pid_t {
+    let converted_path = CString::new(path).unwrap();
+    let v:Vec<*const c_char> = argv.into_iter().map(|string| CString::new(string).unwrap().as_ptr()).collect();
+    v.append(ptr::null() as *const char);
+    unsafe {
+        execve(converted_path.as_ptr(), v, ptr::null());
+    }
+    return 0;
+}
 
 fn main() -> Result<(), Error> {
     let _config = Conf::new(CONFIGURATION.to_string())?;
     println!("Hello, world!");
 
-    let arg: &[u8] = b"hello";
+    let arg = CString::new("hello").unwrap();
     let arg = arg.as_ptr() as *const c_void;
+    let path = "/bin/ls";
+    let args: Vec<String> = vec![String::from("-la"), String::from("-w")];
     unsafe {
-        write(1, arg, 4);
+        let len: size_t = strlen(arg as *const c_char) + 1;
+        println!("len = {}", len);
+        write(1, arg, 6);
+        let child_pid: pid_t = match fork() {
+            0 => exec_child(&path, args),
+            e @ 1..=INT_MAX => e,
+            e => e,
+        };
+        println!("\n{}", child_pid);
     }
     Ok(())
 }
