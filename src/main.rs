1d36a9acfa4e1669d001a14b8df361350e8c7538
Cedric M'Passi <cedricmpassi@gmail.com>
2020-05-11 08:32:23 +0200

    Execve tests in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod task;
+use libc::c_void;
+use libc::write;
 
 mod config;
 use config::Conf;
@@ -8,7 +10,13 @@ type Error = Box<dyn std::error::Error>;
 const CONFIGURATION: &str = "/home/tet/project/taskmaster/example.toml";
 
 fn main() -> Result<(), Error> {
-	let _config = Conf::new(CONFIGURATION.to_string())?;
-	println!("Hello, world!");
-	Ok(())
+    let _config = Conf::new(CONFIGURATION.to_string())?;
+    println!("Hello, world!");
+
+    let arg: &[u8] = b"hello";
+    let arg = arg.as_ptr() as *const c_void;
+    unsafe {
+        write(1, arg, 4);
+    }
+    Ok(())
 }
